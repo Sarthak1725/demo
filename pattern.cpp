df9eab7b76c246062480918d9fe84e51118eb51f
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-07-31 00:51:34 +0530

    new

---
+++ pattern.cpp
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int  main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    
+
+    //for()//outerloop{
+    //for (size_t i = 0; i < count; i++)//inner lop
+    //{
+        /* code */
+    //}
+
+    //for printing in row inner loop is used ---
+
+    // for(int i=1;i<=n;i++){//outer loop for column
+    //     for(int j=1;j<=n;j++){//inner loop for rows
+    //         cout << j <<" ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //  for(int i=0;i<n;i++){
+    //     char ch = 'A';
+    //     for(int j=0;j<n;j++){
+    //         cout << ch;
+    //         ch = ch + 1;//65+1 assic value ek upar
+    //     }
+    //     cout << endl;
+    // }
+    // int num=1;
+
+    // for(int i=0;i<n;i++){
+    //     //int num=1;
+        
+    //     for(int j=0;j<n;j++){
+    //         cout << num;
+    //         num++; // Incrementing the number for each column
+    //     }
+            
+    //     cout << endl;
+    // }
+
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=i-j1;j++){
+            cout <<"*";
+
+        }
+        cout << endl;
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
